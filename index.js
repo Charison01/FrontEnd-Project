e1e632c2d90b5bcc9b4dfe83100a0f22643015da
Kelvin <kelvinkimaru@gmail.com>
2023-04-28 14:34:41 +0300

    Manipulating the DOM

---
+++ index.js
@@ -0,0 +1,12 @@
+//Variables used
+const menu = document.getElementById('menu')
+
+
+//variable hlding the api
+const apiURL = "http://localhost:3000/drinks"
+
+//Fetching data from api
+async function fetchingData(){
+    //Try .. catch errors
+    
+}
